084415c5962884f1a35bfe1672ef633d23da0481
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2023-10-12 10:47:10 +0200

    Se hace funcion para generar un usuario admin apenas se inicializa la bd

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ import colors from "colors";
 import { logger } from "./app/utils/winston.logger.js";
 import Rol from "./app/models/rol.model.js";
 import { createUploadsFolder } from "./app/helpers/image.helpers.js";
+import Usuario from "./app/models/usuario.model.js";
+import bcrypt from "bcryptjs";
 
-// Verifica si el usuario ya fue creado previamente
-let userCreated = false;
 const createRoleIfNotExists = async(roleName) => {
     const role = await Rol.findOne({ where: { nombre: roleName } });
 
@@ -21,6 +21,40 @@ const createRoleIfNotExists = async(roleName) => {
     }
 };
 
+// Verifica si el usuario ya fue creado previamente
+let userCreated = false;
+const syncAndCreateUser = async() => {
+    try {
+        const userRegister = await Usuario.findOne({ where: { email: "admin@incubadora.com" } });
+        if (userRegister) {
+            userCreated = true;
+        }
+
+        // Verifica si el usuario ya fue creado previamente
+        if (!userCreated) {
+            const user = await Usuario.create({
+                nombre: "Admin",
+                apellido: "Incubadora",
+                email: "admin@incubadora.com",
+                password: bcrypt.hashSync("Techtitans1#", 8),
+                avatar: 'https://res.cloudinary.com/fabrizio-dev/image/upload/v1694810559/santex/usuarios/default-user.webp',
+                email_verified_at: new Date(),
+                status: 1
+            });
+            let userRoles = [];
+            const rol = await Rol.findByPk(1);
+            userRoles.push(rol);
+
+            await user.setRols(userRoles);
+            logger.info(`Se genero el usuario admin por defecto en la bd.`);
+            console.log('Se genero el usuario admin por defecto en la bd');
+            userCreated = true;
+        }
+    } catch (error) {
+        console.error('Error al sincronizar y crear el usuario:', error);
+    }
+};
+
 
 sequelize
     .sync({ force: false })
@@ -31,6 +65,7 @@ sequelize
         createRoleIfNotExists("docente");
         createRoleIfNotExists("estudiante");
         createUploadsFolder();
+        syncAndCreateUser()
         app.listen(app.get('port'), () => {
             console.log(`El servidor se ejecuta en el puerto: ${app.get('port')} sin problemas. En el entorno de: ${app.get('env')}`.green)
             logger.debug(`El servidor se ejecuta en el puerto: ${app.get('port')} sin problemas. En el entorno de: ${app.get('env')}`)
